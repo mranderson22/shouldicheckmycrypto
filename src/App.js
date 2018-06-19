6d19d7bcc34b4d705db80ad8809f5108b3226729
Eric Anderson <anderson.eda@gmail.com>
2018-06-19 18:13:54 -0400

    further setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
+import Header from './Components/Header';
+import style from './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>shouldicheckmycrypto</h1>
+        <Header />
       </div>
     );
   }
