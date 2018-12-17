908fc1e3da2e825033c5ab5eee7595ad91db231b
Eric Anderson <anderson.eda@gmail.com>
2018-12-17 15:50:49 -0500

    server issues

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var path = require('path');
 var express = require('express');
+var cors = require('cors')
 
 var app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist')), cors());
 app.set('port', process.env.PORT || 80);
 
-var server = app.listen(app.get('port'), function() {
+const server = app.listen(app.get('port'), function() {
   console.log('listening on port ', server.address().port);
 });
