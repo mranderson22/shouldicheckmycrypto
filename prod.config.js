ad5207996eed9a3e90a2c37b73afc4a410fc3264
Eric Anderson <anderson.eda@gmail.com>
2019-05-07 16:46:43 -0400

    prod.config

--- prod.config.js
+++ prod.config.js
@@ -1,53 +1,77 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
+module.exports = (env) => {
+  const isProduction = env === 'production';
 
-  entry: ['./src/index.js', 'babel-polyfill'],
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'index_bundle.js'
-  },
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      filename: 'index_bundle.js'
+    },
 
-  module: {
-    rules: [
-      {
-        test: /\.(jpg|png|svg|ico)$/,
-        loader: 'url-loader'
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
+    module: {
+      rules: [
+        {
+          test: /\.(jpg|png|svg|ico)$/,
+          loader: 'url-loader',
+          options: {
+            limit: 2500000000
+          }
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
+        },
+        {
+          test: /\.s?css$/,
+          use: [
+            {
+              loader: 'style-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'resolve-url-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
         }
-      },
-      {
-        test: /\.s?css$/,
-        
-        use: [{
-          loader: 'style-loader'
-        },
-        {
-          loader: 'css-loader'
-        },
-        {
-          loader: 'resolve-url-loader'
-        },
-        {
-          loader: 'sass-loader'
-        }]
-      }
+      ]
+    },
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify('production')
+      }),
+      new HtmlWebpackPlugin({
+        template: './src/index.html'
+      }),
+      new MiniCssExtractPlugin({
+        filename: '[name].css'
+      })
+
     ]
-  },
-  devtool: 'inline-source-map',
-
-  plugins: [
-
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
-    })
-
-
-  ]
+  };
 };
